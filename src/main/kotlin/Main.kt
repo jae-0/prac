eca5ab91734868362b401beb81107b5476f531ff
YOON <wodud0747@naver.com>
2024-12-09 16:51:04 +0900

    api gateway 통신 test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,41 @@ import com.machinezoo.sourceafis.FingerprintTemplate
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.dynamodb.model.GetItemRequest
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.util.Base64
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.Scanner
+
+data class FingerprintData(
+    val studentId: String,
+    val fingerprint: String
+)
+
 
 class Main {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
             /**** 여기에 DB 객체 받아오도록 설정 필요 ****/
+            val apiGatewayEvent = """
+                {
+                    "studentId": "exampleStudentId",
+                    "fingerprint": "base64EncodedFingerprintData"
+                }
+            """.trimIndent()
+
+            // JSON 데이터를 데이터 클래스로 변환
+            val objectMapper = jacksonObjectMapper()
+            val requestData: FingerprintData = objectMapper.readValue(apiGatewayEvent, FingerprintData::class.java)
             // DynamoDB 클라이언트 생성
             val dynamoDbClient = DynamoDbClient.create()
 
              // 가져올 테이블 이름과 키 값 설정
             val tableName = "Fingerprint-db"
-            val primaryKeyName = "YourPrimaryKey"
-            val primaryKeyValue = "KeyValue"
+            val primaryKeyName = requestData.studentId
+            val primaryKeyValue = requestData.fingerprint
 
             /// GetItem 요청 생성
             val getItemRequest = GetItemRequest.builder()
@@ -40,7 +58,7 @@ class Main {
                 val images = item["Images"]?.m() ?: emptyMap()
 
                 // 비교할 이미지 decode해서 비교할 준비
-                val comparedecodedImage = Base64.getDecoder().decode(primaryKeyName)
+                val comparedecodedImage = Base64.getDecoder().decode(primaryKeyValue)
                 val comparededecodedImage = FingerprintTemplate().dpi(500.0).create(comparedecodedImage)
         
                 // 2. DynamoDB 이미지 데이터 디코딩 및 배열에 추가
